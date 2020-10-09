acdc5c48cc011293d719bc7865543e50d51e5263
AnneJongbloed <70448341+AnneJongbloed@users.noreply.github.com>
2020-10-09 11:28:42 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
-// JavaScript Document
+var navbar = document.querySelector("nav")
+var ham = document.querySelector("button")
+
+ham.addEventListener("click", toggleHamburger)
+
+function toggleHamburger(){
+  navbar.classList.toggle("showNav")
+  ham.classList.toggle("showClose")
+}
+
+var menuLinks = document.querySelectorAll("a")
+
+menuLinks.forEach( 
+  function(menuLink) { 
+    menuLink.addEventListener("click", toggleHamburger) 
+  }
+)
